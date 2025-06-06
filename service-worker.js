fa60c7a6c1726c0bcad533e9460bc59bede210be
hxj <7994086+huangjunxue@user.noreply.gitee.com>
2025-06-06 16:34:51 +0800

    construct s t min

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("install",e=>{e.waitUntil(self.skipWaiting())});self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim())});self.addEventListener("message",e=>{e?.data?.type==="SKIP_WAITING"&&self.skipWaiting()});
